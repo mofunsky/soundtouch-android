430dc9add9da8387637d746b6b6e29c6cf40668d
svenoaks <svenoaks@gmail.com>
2014-04-30 23:30:48 -0400

    looping

--- src/com/smp/soundtouchandroid/SoundTouchPlayable.java
+++ src/com/smp/soundtouchandroid/SoundTouchPlayable.java
@@ -3,20 +3,16 @@ package com.smp.soundtouchandroid;
 import static com.smp.soundtouchandroid.Constants.*;
 
 import java.io.IOException;
-import java.util.Arrays;
-
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Build;
 import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
 
 public class SoundTouchPlayable implements Runnable
 {
 	private static final long NOT_SET = Long.MIN_VALUE;
-	
+
 	private Object pauseLock;
 	private Object trackLock;
 	private Object decodeLock;
@@ -29,6 +25,7 @@ public class SoundTouchPlayable implements Runnable
 	private int id;
 	private boolean bypassSoundTouch;
 	
+	private volatile long bytesWritten;
 	private volatile long loopStart = NOT_SET;
 	private volatile long loopEnd = NOT_SET;
 	private volatile AudioTrack track;
@@ -36,9 +33,15 @@ public class SoundTouchPlayable implements Runnable
 
 	public interface OnProgressChangedListener
 	{
-		void onProgressChanged(int track, double currentPercentage, long position);
+		void onProgressChanged(int track, double currentPercentage,
+				long position);
 
 		void onTrackEnd(int track);
+	}
+
+	public long getBytesWritten()
+	{
+		return bytesWritten;
 	}
 
 	public int getChannels()
@@ -68,11 +71,12 @@ public class SoundTouchPlayable implements Runnable
 	{
 		return soundTouch.getPitchSemi();
 	}
-	
+
 	public long getPlaybackLimit()
 	{
 		return loopEnd;
 	}
+
 	public int getSamplingRate()
 	{
 		return soundTouch.getSamplingRate();
@@ -82,17 +86,31 @@ public class SoundTouchPlayable implements Runnable
 	{
 		return track.getAudioSessionId();
 	}
-
+	
+	public long getSoundTouchBufferSize()
+	{
+		return soundTouch.getOutputBufferSize();
+	}
+	
+	public long getAudioTrackBufferSize()
+	{
+		long playbackHead = track.getPlaybackHeadPosition() & 0xffffffffL; 
+		return bytesWritten - playbackHead 
+				* DEFAULT_BYTES_PER_SAMPLE * getChannels();
+	}
+	
 	public float getTempo()
 	{
 		return soundTouch.getTempo();
 	}
 
-	public long getLoopEnd() {
+	public long getLoopEnd()
+	{
 		return loopEnd;
 	}
 
-	public long getLoopStart() {
+	public long getLoopStart()
+	{
 		return loopStart;
 	}
 
@@ -101,7 +119,7 @@ public class SoundTouchPlayable implements Runnable
 		return track.getState() == AudioTrack.STATE_INITIALIZED;
 	}
 
-	public boolean isLooping() 
+	public boolean isLooping()
 	{
 		return loopStart != NOT_SET && loopEnd != NOT_SET;
 	}
@@ -116,18 +134,21 @@ public class SoundTouchPlayable implements Runnable
 		this.bypassSoundTouch = bypassSoundTouch;
 	}
 
-	public void setLoopEnd(long loopEnd) {
+	public void setLoopEnd(long loopEnd)
+	{
 		long pd = decoder.getPlayedDuration();
 		if (loopStart != NOT_SET && pd <= loopStart)
-			throw new SoundTouchAndroidException("Invalid Loop Time, loop start must be < loop end");
+			throw new SoundTouchAndroidException(
+					"Invalid Loop Time, loop start must be < loop end");
 		this.loopEnd = loopEnd;
 	}
 
-	public void setLoopStart(long loopStart) 
-	{	
+	public void setLoopStart(long loopStart)
+	{
 		long pd = decoder.getPlayedDuration();
 		if (loopEnd != NOT_SET && pd >= loopEnd)
-			throw new SoundTouchAndroidException("Invalid Loop Time, loop start must be < loop end");
+			throw new SoundTouchAndroidException(
+					"Invalid Loop Time, loop start must be < loop end");
 		this.loopStart = loopStart;
 	}
 
@@ -135,7 +156,7 @@ public class SoundTouchPlayable implements Runnable
 	{
 		soundTouch.setPitchSemi(pitchSemi);
 	}
-	
+
 	public void setTempo(float tempo)
 	{
 		soundTouch.setTempo(tempo);
@@ -157,21 +178,21 @@ public class SoundTouchPlayable implements Runnable
 		}
 	}
 
-	public SoundTouchPlayable(OnProgressChangedListener playbackListener, String fileName, int id, float tempo, float pitchSemi)
+	public SoundTouchPlayable(OnProgressChangedListener playbackListener,
+			String fileName, int id, float tempo, float pitchSemi)
 			throws IOException
 	{
 		this(fileName, id, tempo, pitchSemi);
 		this.playbackListener = playbackListener;
 	}
 
-	public SoundTouchPlayable(String fileName, int id, float tempo, float pitchSemi)
-			throws IOException, SoundTouchAndroidException
+	public SoundTouchPlayable(String fileName, int id, float tempo,
+			float pitchSemi) throws IOException, SoundTouchAndroidException
 	{
 		if (Build.VERSION.SDK_INT >= 16)
 		{
 			decoder = new MediaCodecAudioDecoder(fileName);
-		}
-		else
+		} else
 		{
 			decoder = new JLayerAudioDecoder(fileName);
 		}
@@ -215,13 +236,11 @@ public class SoundTouchPlayable implements Runnable
 				}
 				decoder.resetEOS();
 			}
-		}
-		catch (SoundTouchAndroidException e)
+		} catch (SoundTouchAndroidException e)
 		{
 			// need to notify...something?
 			e.printStackTrace();
-		}
-		finally
+		} finally
 		{
 			soundTouch.clearBuffer();
 
@@ -237,6 +256,12 @@ public class SoundTouchPlayable implements Runnable
 			}
 		}
 	}
+	
+	public void clearLoopPoints()
+	{
+		loopStart = NOT_SET;
+		loopEnd = NOT_SET;
+	}
 
 	public void seekTo(double percentage, boolean shouldFlush) // 0.0 - 1.0
 	{
@@ -247,7 +272,8 @@ public class SoundTouchPlayable implements Runnable
 	public void seekTo(long timeInUs, boolean shouldFlush)
 	{
 		if (timeInUs < 0 || timeInUs > decoder.getDuration())
-			throw new SoundTouchAndroidException("" + timeInUs + " Not a valid seek time.");
+			throw new SoundTouchAndroidException("" + timeInUs
+					+ " Not a valid seek time.");
 
 		if (shouldFlush)
 		{
@@ -255,6 +281,7 @@ public class SoundTouchPlayable implements Runnable
 			synchronized (trackLock)
 			{
 				track.flush();
+				bytesWritten = 0;
 			}
 			soundTouch.clearBuffer();
 		}
@@ -312,8 +339,7 @@ public class SoundTouchPlayable implements Runnable
 				try
 				{
 					pauseLock.wait();
-				}
-				catch (InterruptedException e)
+				} catch (InterruptedException e)
 				{
 				}
 			}
@@ -324,86 +350,89 @@ public class SoundTouchPlayable implements Runnable
 	{
 		int bytesReceived = 0;
 		byte[] input = null;
-	
+
 		do
 		{
 			pauseWait();
 			if (finished)
 				break;
-			
-			if (isLooping() && decoder.getPlayedDuration() >= loopEnd) 
+
+			if (isLooping() && decoder.getPlayedDuration() >= loopEnd)
+			{
 				seekTo(loopStart, false);
-			
+			}
+
 			if (soundTouch.getOutputBufferSize() <= MAX_OUTPUT_BUFFER_SIZE)
 			{
 				synchronized (decodeLock)
 				{
 					input = decoder.decodeChunk();
-	
-					if (playbackListener != null)
-					{
-						handler.post(new Runnable()
-						{
-	
-							@Override
-							public void run()
-							{
-								long pd = decoder.getPlayedDuration();
-								long d = decoder.getDuration();
-								double cp = pd == 0 ? 0 : (double) pd / d;
-								playbackListener.onProgressChanged(id, cp, pd);
-							}
-						});
-					}
 				}
-			
+				sendProgressUpdate();
 				processChunk(input, true);
-			}
-			else
+			} else
 			{
 				processChunk(input, false);
 			}
-		}
-		while (!decoder.sawOutputEOS());
-	
+		} while (!decoder.sawOutputEOS());
+
 		soundTouch.finish();
-	
+
 		do
 		{
 			if (finished)
 				break;
 			bytesReceived = processChunk(input, false);
-		}
-		while (bytesReceived > 0);
+		} while (bytesReceived > 0);
 	}
 
-	private int processChunk(final byte[] input, boolean putBytes) throws SoundTouchAndroidException
+	private void sendProgressUpdate()
+	{
+		if (playbackListener != null)
+		{
+			handler.post(new Runnable()
+			{
+
+				@Override
+				public void run()
+				{
+					long pd = decoder.getPlayedDuration();
+					long d = decoder.getDuration();
+					double cp = pd == 0 ? 0 : (double) pd / d;
+					playbackListener.onProgressChanged(id, cp, pd);
+				}
+			});
+		}
+	}
+
+	private int processChunk(final byte[] input, boolean putBytes)
+			throws SoundTouchAndroidException
 	{
 		int bytesReceived = 0;
-	
+
 		if (input != null)
 		{
 			if (bypassSoundTouch)
 			{
 				bytesReceived = input.length;
-			}
-			else
+			} else
 			{
 				if (putBytes)
 					soundTouch.putBytes(input);
-				//Log.d("bytes", String.valueOf(input.length));
+				// Log.d("bytes", String.valueOf(input.length));
 				bytesReceived = soundTouch.getBytes(input);
 			}
 			synchronized (trackLock)
 			{
-				track.write(input, 0, bytesReceived);
+				bytesWritten += track.write(input, 0, bytesReceived);
 			}
-	
+
 		}
 		return bytesReceived;
 	}
 
-	private void setupAudio(int id, float tempo, float pitchSemi) throws IOException
+	private void setupAudio(int id, float tempo, float pitchSemi)
+			throws IOException
 	{
 		int channels = decoder.getChannels();
 		int samplingRate = decoder.getSamplingRate();
@@ -415,11 +444,14 @@ public class SoundTouchPlayable implements Runnable
 		else if (channels == 2) // stereo
 			channelFormat = AudioFormat.CHANNEL_OUT_STEREO;
 		else
-			throw new SoundTouchAndroidException("Valid channel count is 1 or 2");
+			throw new SoundTouchAndroidException(
+					"Valid channel count is 1 or 2");
 
-		soundTouch = new SoundTouch(id, channels, samplingRate, DEFAULT_BYTES_PER_SAMPLE, tempo, pitchSemi);
+		soundTouch = new SoundTouch(id, channels, samplingRate,
+				DEFAULT_BYTES_PER_SAMPLE, tempo, pitchSemi);
 
-		track = new AudioTrack(AudioManager.STREAM_MUSIC, samplingRate, channelFormat,
-				AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE_TRACK, AudioTrack.MODE_STREAM);
+		track = new AudioTrack(AudioManager.STREAM_MUSIC, samplingRate,
+				channelFormat, AudioFormat.ENCODING_PCM_16BIT,
+				BUFFER_SIZE_TRACK, AudioTrack.MODE_STREAM);
 	}
 }
